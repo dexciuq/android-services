51cbbc513088896a9db77aa893b3788357021664
dimok <zhasulanov04@list.ru>
2023-12-28 19:08:57 +0600

    Create notification for service

--- app/src/main/java/com/dexciuq/android_services/MainActivity.kt
+++ app/src/main/java/com/dexciuq/android_services/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.dexciuq.android_services
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
 import com.dexciuq.android_services.databinding.ActivityMainBinding
 import timber.log.Timber
 
@@ -18,5 +23,36 @@ class MainActivity : AppCompatActivity() {
             val intent = MyService.newIntent(this@MainActivity, 25)
             startService(intent)
         }
+
+        binding.foregroundService.setOnClickListener {
+            showNotification()
+        }
+    }
+
+    private fun showNotification() {
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle("Title")
+            .setContentText("Text")
+            .setSmallIcon(androidx.core.R.drawable.notification_bg)
+            .build()
+
+        notificationManager.notify(1, notification)
+    }
+
+    companion object {
+        private const val CHANNEL_ID = "channel_id"
+        private const val CHANNEL_NAME = "Service Notification"
+
     }
 }
